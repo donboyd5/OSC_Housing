09ffaad36a8632da6ed72a440e7929f16d16b12b
Don Boyd <donboyd5@gmail.com>
2022-10-17 12:02:57 -0400

    post-Portugal update

--- r/functions.R
+++ r/functions.R
@@ -7,7 +7,7 @@ ftabranks <- function(tabdata,
               tabsubtitle=NULL,
               keepcounty=FALSE){
   
-  mainvars <- c("alltenure_allcost",
+  mainvars <- c("allunits",
                 "own_pct", "rent_pct", "alltenure_pct", 
                 "ownrank", "rentrank", "alltenurerank",
                 "renter_share")
@@ -29,11 +29,11 @@ ftabranks <- function(tabdata,
       subtitle = tabsubtitle
     ) |> 
     tab_spanner(columns = c(own_pct, rent_pct, alltenure_pct),
-                label=html("Percent of household<br>units that are cost-burdened")) |> 
+                label=html("Percent of households<br>with this problem")) |> 
     tab_spanner(columns = c(ownrank, rentrank, alltenurerank),
                 label="Rank") |> 
     cols_label(stub=stubhead,
-               alltenure_allcost=html("# of<br>occupied units"),
+               allunits=html("# of<br>occupied units"),
                own_pct="Owners",
                rent_pct="Renters",
                alltenure_pct="All",
@@ -42,7 +42,7 @@ ftabranks <- function(tabdata,
                alltenurerank="All",
                renter_share=html("Renter households as<br>% of all households")) |> 
     fmt_percent(columns=c(own_pct, rent_pct, alltenure_pct, renter_share), decimals=1) |> 
-    fmt_number(columns=c(alltenure_allcost, ownrank, rentrank, alltenurerank),
+    fmt_number(columns=c(allunits, ownrank, rentrank, alltenurerank),
                decimals=0) |> 
     tab_source_note(source_note = "Source: HUD 5-year CHAS ending 2019")
   
