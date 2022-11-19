e40f1000853b4c3ff759df31f5b3e792e9470ff4
Don Boyd <donboyd5@gmail.com>
2022-11-19 05:35:59 -0500

    update gitignore

--- r/constants.R
+++ r/constants.R
@@ -1,1 +1,8 @@
-tabdir <- path_home("Documents", "R_projects", "OSC_Housing", "report", "gtables")
+# tabdir <- path_home("Documents", "R_projects", "OSC_Housing", "report", "gtables")
+tabdir <- path("e:/", "R_projects", "OSC_Housing", "report", "gtables")
+
+legend_none <- theme(legend.position = "None")
+legend_notitle <- theme(legend.title = element_blank())
+caption_left <- theme(plot.caption = element_text(hjust = 0))
+
+nycfips <- c("36005", "36047", "36061", "36081", "36085")
